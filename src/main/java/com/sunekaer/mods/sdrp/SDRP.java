fe47157243d65352ff29da9f04ca539464c61c87
Sunekaer <git@sunekaer.com>
2022-06-25 13:05:28 +0200

    Started on rewrite

--- src/main/java/com/sunekaer/mods/sdrp/SDRP.java
+++ src/main/java/com/sunekaer/mods/sdrp/SDRP.java
@@ -4,20 +4,14 @@ import com.mojang.realmsclient.RealmsMainScreen;
 import com.sunekaer.mods.sdrp.config.Config;
 import com.sunekaer.mods.sdrp.discord.DiscordRichPresence;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.client.gui.screens.multiplayer.JoinMultiplayerScreen;
 import net.minecraft.client.gui.screens.worldselection.SelectWorldScreen;
 import net.minecraft.client.player.AbstractClientPlayer;
-import net.minecraft.realms.RealmsScreen;
-import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ClientPlayerChangeGameTypeEvent;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -37,8 +31,8 @@ public class SDRP {
         DistExecutor.runWhenOn(Dist.CLIENT, () -> () -> {
             ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, Config.configSpec);
             FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-            MinecraftForge.EVENT_BUS.addListener(this::initGui);
-            MinecraftForge.EVENT_BUS.addListener(this::entityJoinWorld);
+//            MinecraftForge.EVENT_BUS.addListener(this::initGui);
+//            MinecraftForge.EVENT_BUS.addListener(this::entityJoinWorld);
         });
         DistExecutor.runWhenOn(Dist.DEDICATED_SERVER, () -> () -> LOGGER.warn("This is a client only mod!"));
     }
@@ -47,28 +41,28 @@ public class SDRP {
         DiscordRichPresence.start();
     }
 
-    private void initGui(ScreenEvent.InitScreenEvent event) {
-        if (!DiscordRichPresence.isEnabled()) {
-            return;
-        }
+//    private void initGui(ScreenEvent.InitScreenEvent event) {
+//        if (!DiscordRichPresence.isEnabled()) {
+//            return;
+//        }
+//
+//        if (event.getScreen() instanceof TitleScreen || event.getScreen() instanceof JoinMultiplayerScreen || event.getScreen() instanceof SelectWorldScreen || event.getScreen() instanceof RealmsMainScreen) {
+//            final DiscordRichPresence.State state = DiscordRichPresence.getCurrent();
+//            if (state != DiscordRichPresence.map.get("menu")) {
+//                DiscordRichPresence.setState(DiscordRichPresence.map.get("menu"));
+//            }
+//        }
+//    }
 
-        if (event.getScreen() instanceof TitleScreen || event.getScreen() instanceof JoinMultiplayerScreen || event.getScreen() instanceof SelectWorldScreen || event.getScreen() instanceof RealmsMainScreen) {
-            final DiscordRichPresence.State state = DiscordRichPresence.getCurrent();
-            if (state != DiscordRichPresence.map.get("menu")) {
-                DiscordRichPresence.setState(DiscordRichPresence.map.get("menu"));
-            }
-        }
-    }
-
-    private void entityJoinWorld(EntityJoinWorldEvent event) {
-      if (!DiscordRichPresence.isEnabled()) {
-            return;
-        }
-        if (event.getEntity() instanceof AbstractClientPlayer){
-            //assert Minecraft.getInstance().player != null;
-            if (event.getEntity().getUUID().equals(Minecraft.getInstance().player.getUUID())){
-                DiscordRichPresence.setDimension(event.getEntity().getLevel());
-            }
-        }
-    }
+//    private void entityJoinWorld(EntityJoinWorldEvent event) {
+//      if (!DiscordRichPresence.isEnabled()) {
+//            return;
+//        }
+//        if (event.getEntity() instanceof AbstractClientPlayer){
+//            //assert Minecraft.getInstance().player != null;
+//            if (event.getEntity().getUUID().equals(Minecraft.getInstance().player.getUUID())){
+//                DiscordRichPresence.setDimension(event.getEntity().getLevel());
+//            }
+//        }
+//    }
 }
--- src/main/java/com/sunekaer/mods/sdrp/discord/DiscordRichPresence.java
+++ src/main/java/com/sunekaer/mods/sdrp/discord/DiscordRichPresence.java
@@ -1,16 +1,20 @@
 package com.sunekaer.mods.sdrp.discord;
 
 import com.sunekaer.mods.sdrp.config.Config;
-import com.sunekaer.mods.sdrp.discord.discordipc.IPCClient;
-import com.sunekaer.mods.sdrp.discord.discordipc.entities.RichPresence;
-import com.sunekaer.mods.sdrp.discord.discordipc.exceptions.NoDiscordClientException;
 
+import de.jcm.discordgamesdk.Core;
+import de.jcm.discordgamesdk.CreateParams;
+import de.jcm.discordgamesdk.activity.Activity;
+import de.jcm.discordgamesdk.activity.ActivityType;
 import net.minecraft.Util;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.world.level.Level;
+import org.apache.commons.lang3.SystemUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
+import java.time.Instant;
 import java.time.OffsetDateTime;
 import java.util.HashMap;
 import java.util.concurrent.Executors;
@@ -22,109 +26,168 @@ public class DiscordRichPresence {
 
     private static final OffsetDateTime TIME = OffsetDateTime.now();
     private static State currentState;
-    private static final IPCClient CLIENT = new IPCClient(Config.CONFIG.clientID.get());
+
+//    private static Core core;
+    private static Activity activity;
     private static boolean isEnabled = false;
     private static int errorCount = 0;
     private static final ScheduledExecutorService EXECUTOR_SERVICE = Executors.newSingleThreadScheduledExecutor();
 
-    private static final Runnable TIMER_TASK = () -> {
-        if (currentState != null && CLIENT.getStatus() == IPCClient.Status.CONNECTED) {
-            setState(currentState);
-        }
-    };
-
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            LOGGER.info("Shutting down Discord Rich Presence client");
-            stop();
-            try {
-                EXECUTOR_SERVICE.shutdown();
-            } catch (RuntimeException error) {
-                LOGGER.error("Error shutting down Discord Rich Presence timer", error);
-            }
-        }, "DiscordRP Stop"));
-    }
+//    private static final Runnable TIMER_TASK = () -> {
+//        if (currentState != null && CLIENT.getStatus() == IPCClient.Status.CONNECTED) {
+//            setState(currentState);
+//        }
+//    };
+//
+//    static {
+//        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+//            LOGGER.info("Shutting down Discord Rich Presence client");
+//            stop();
+//            try {
+//                EXECUTOR_SERVICE.shutdown();
+//            } catch (RuntimeException error) {
+//                LOGGER.error("Error shutting down Discord Rich Presence timer", error);
+//            }
+//        }, "DiscordRP Stop"));
+//    }
 
     public static void start() {
         Util.backgroundExecutor().execute(() -> {
             try {
-                CLIENT.connect();
-                EXECUTOR_SERVICE.scheduleAtFixedRate(TIMER_TASK, 1000, 1000 * 120, TimeUnit.MILLISECONDS);
-                isEnabled = true;
+                String fileName;
+                if (SystemUtils.IS_OS_WINDOWS)
+                    fileName = "./sdk/discord_game_sdk.dll";
+                else if (SystemUtils.IS_OS_MAC)
+                    fileName = "./sdk/discord_game_sdk.dylib";
+                else if (SystemUtils.IS_OS_LINUX)
+                    fileName = "./sdk/discord_game_sdk.so";
+                else
+                    throw new RuntimeException("cannot determine OS type: " + System.getProperty("os.name"));
 
-                final State state = DiscordRichPresence.getCurrent();
-                if (state == null || state != map.get("loading")) {
-                    setState(DiscordRichPresence.map.get("loading"));
+                System.out.println(fileName);
+
+                Core.initDiscordNative(fileName);
+
+                try(CreateParams params = new CreateParams())
+                {
+                    params.setClientID(Config.CONFIG.clientID.get());
+                    params.setFlags(CreateParams.getDefaultFlags());
+
+                    try(Core core = new Core(params))
+                    {
+                        activity = new Activity();
+                        activity.setState("World!");
+
+                        activity.setDetails("Hello");
+
+                        activity.setType(ActivityType.PLAYING);
+
+                        activity.party().setID("1234");
+
+                        activity.party().size().setCurrentSize(10);
+                        activity.party().size().setMaxSize(100);
+
+                        activity.timestamps().setStart(Instant.now());
+                        activity.timestamps().setEnd(Instant.now().plusSeconds(16));
+
+                        activity.assets().setLargeImage("test");
+                        activity.assets().setLargeText("Just a test!");
+
+                        activity.assets().setSmallImage("test");
+                        activity.assets().setSmallText("It's a TEST!!!");
+
+                        activity.secrets().setMatchSecret("match");
+                        activity.secrets().setJoinSecret("join");
+                        activity.secrets().setSpectateSecret("spectate");
+
+                        activity.setInstance(true);
+                    } catch (Error er) {
+                        LOGGER.error("Failed to create Core");
+                    }
+
+
+//                    core.activityManager().updateActivity(activity, result -> {
+//
+//                    });
                 }
+
+//
+//                EXECUTOR_SERVICE.scheduleAtFixedRate(TIMER_TASK, 1000, 1000 * 120, TimeUnit.MILLISECONDS);
+//                isEnabled = true;
+//
+//                final State state = DiscordRichPresence.getCurrent();
+//                if (state == null || state != map.get("loading")) {
+//                    setState(DiscordRichPresence.map.get("loading"));
+//                }
 
                 LOGGER.info("Discord client found and connected.");
-            } catch (NoDiscordClientException ex) {
-                LOGGER.info("Discord client was not found.");
+            } catch (RuntimeException e) {
+                throw new RuntimeException(e);
             }
         });
     }
 
-    public static void stop() {
-        try {
-            CLIENT.close();
-        } catch (Exception ex) {
-            LOGGER.error("Error closing Discord connection.", ex);
-        }
-        errorCount = 0;
-        isEnabled = false;
-        LOGGER.info("Discord client closed.");
-    }
+//    public static void stop() {
+//        try {
+//            CLIENT.close();
+//        } catch (Exception ex) {
+//            LOGGER.error("Error closing Discord connection.", ex);
+//        }
+//        errorCount = 0;
+//        isEnabled = false;
+//        LOGGER.info("Discord client closed.");
+//    }
 
-    public static void setDimension(Level level) {
-        State dim = map.get(level.dimension().toString());
-        if (dim != null) {
-            setState(dim);
-        } else {
-            String name = I18n.get("sdrp." + level.dimension().location().getPath());
-            String in = I18n.get("sdrp." + level.dimension().location().getPath() + ".in");
-            String key = level.dimension().location().getPath();
+//    public static void setDimension(Level level) {
+//        State dim = map.get(level.dimension().toString());
+//        if (dim != null) {
+//            setState(dim);
+//        } else {
+//            String name = I18n.get("sdrp." + level.dimension().location().getPath());
+//            String in = I18n.get("sdrp." + level.dimension().location().getPath() + ".in");
+//            String key = level.dimension().location().getPath();
+//
+//            State dim2 = new State(in,  name, key);
+//            setState(dim2);
+//        }
+//    }
 
-            State dim2 = new State(in,  name, key);
-            setState(dim2);
-        }
-    }
-
-    public static void setState(State state) {
-        if (state == null) {
-            return;
-        }
-
-        Util.backgroundExecutor().execute(() -> {
-            currentState = state;
-            RichPresence.Builder builder = new RichPresence.Builder();
-
-            builder.setState(state.message);
-            builder.setStartTimestamp(TIME);
-            String name = I18n.get("sdrp.logo");
-            builder.setLargeImage("logo", name);
-            builder.setSmallImage(state.imageKey, state.imageName);
-            try {
-                CLIENT.sendRichPresence(builder.build());
-            } catch (Exception ex) {
-                LOGGER.error(ex);
-                try {
-                    CLIENT.connect();
-                    errorCount = 0;
-                    CLIENT.sendRichPresence(builder.build());
-                } catch (Exception ex2) {
-                    try {
-                        CLIENT.close();
-                    } catch (Exception ex3) {
-                    }
-                    errorCount++;
-                    if (errorCount > 10) {
-                        LOGGER.info("DiscordRP connection failed.");
-                        stop();
-                    }
-                }
-            }
-        });
-    }
+//    public static void setState(State state) {
+//        if (state == null) {
+//            return;
+//        }
+//
+//        Util.backgroundExecutor().execute(() -> {
+//            currentState = state;
+//            RichPresence.Builder builder = new RichPresence.Builder();
+//
+//            builder.setState(state.message);
+//            builder.setStartTimestamp(TIME);
+//            String name = I18n.get("sdrp.logo");
+//            builder.setLargeImage("logo", name);
+//            builder.setSmallImage(state.imageKey, state.imageName);
+//            try {
+//                CLIENT.sendRichPresence(builder.build());
+//            } catch (Exception ex) {
+//                LOGGER.error(ex);
+//                try {
+//                    CLIENT.connect();
+//                    errorCount = 0;
+//                    CLIENT.sendRichPresence(builder.build());
+//                } catch (Exception ex2) {
+//                    try {
+//                        CLIENT.close();
+//                    } catch (Exception ex3) {
+//                    }
+//                    errorCount++;
+//                    if (errorCount > 10) {
+//                        LOGGER.info("DiscordRP connection failed.");
+//                        stop();
+//                    }
+//                }
+//            }
+//        });
+//    }
 
     public static boolean isEnabled() {
         return isEnabled;
